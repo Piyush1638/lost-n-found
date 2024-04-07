e16b27b5fb6cfd4d1de1a4ac399a88dc65e053d2
Piyush Kumar Singh <21bcs1638@cuchd.in>
2024-04-07 20:08:55 +0530

    commit

--- src/app/[category]/[id]/page.tsx
+++ src/app/[category]/[id]/page.tsx
@@ -5,7 +5,7 @@ const page = ({params}:any) => {
   return (
     <main className='min-h-screen'>
       <div className='flex items-center justify-center'>
-
+           <h1>Here is the product</h1>
       </div>
     </main>
   )
